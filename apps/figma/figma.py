38273ed4cf954e2ee6d7f196d6839c36834311bc
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-01-22 13:12:24 +0000

    delete unneeded

--- apps/figma/figma.py
+++
@@ -1,8 +0,0 @@
-from talon import Module
-
-mod = Module()
-
-mod.apps.figma = r"""
-os: windows
-and app.exe: Figma.exe
-"""
--- apps/figma/figma.talon
+++
@@ -1,4 +0,0 @@
-app: figma
--
-tag(): user.zoom
-tag(): user.insert_paste_disabled
