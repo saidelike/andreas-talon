ffac3c8da826b482de4715507ab2763cb5e2218f
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-01-29 13:03:16 +0000

    remove Swedish

--- core/vocabulary/vocabulary.py
+++ core/vocabulary/vocabulary.py
@@ -9,26 +9,13 @@ mod.list("vocabulary", "Additional vocabulary words")
 
 ctx_en = Context()
 
-ctx_sv = Context()
-ctx_sv.matches = r"""
-language: sv
-"""
-
-
 phrase_replacer_en = None
-phrase_replacer_sv = None
 
 
 @ctx_en.action_class("dictate")
 class DictateActionsEn:
     def replace_words(words: Sequence[str]) -> Sequence[str]:
         return phrase_replacer_en.replace(words)
-
-
-@ctx_sv.action_class("dictate")
-class DictateActionsSv:
-    def replace_words(words: Sequence[str]) -> Sequence[str]:
-        return phrase_replacer_sv.replace(words)
 
 
 @mod.action_class
@@ -52,20 +39,11 @@ def words_to_replace_en_update(csv_dict: dict):
     phrase_replacer_en = PhraseReplacer(csv_dict)
 
 
-def words_to_replace_sv_update(csv_dict: dict):
-    global phrase_replacer_sv
-    phrase_replacer_sv = PhraseReplacer(csv_dict)
-
-
 def on_ready():
     dir = Path(__file__).parent
     actions.user.watch_csv_as_dict(
         dir / "words_to_replace_en.csv",
         words_to_replace_en_update,
-    )
-    actions.user.watch_csv_as_dict(
-        dir / "words_to_replace_sv.csv",
-        words_to_replace_sv_update,
     )
 
 
