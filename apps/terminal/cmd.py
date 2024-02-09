e269c505f56517a05c4ac211b3fde0b52be44f95
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-02-09 06:16:18 +0000

    Basic cmd support to use vim terminal / neovim

--- apps/terminal/cmd.py
+++ apps/terminal/cmd.py
@@ -1,17 +1,46 @@
-from talon import Module, Context
+from talon import Module, Context, actions
 
 mod = Module()
 ctx = Context()
 
 # we define what it is to be a "cmd" app
+# TODO: for now we enable cmd when we are in vim terminal
 mod.apps.cmd = r"""
 os: windows
-win.title: Command Prompt
+and win.title: Command Prompt
+
 os: windows
-win.title: /C:\\Windows\\System32\\cmd.exe/
+and win.title: /C:\\Windows\\System32\\cmd.exe/
+
+os: windows
+and tag: user.vim_terminal
 """
 
 # this context is only active when the above "cmd" app is enabled
 ctx.matches = r"""
 app: cmd
 """
+
+
+# these are "user" actions
+@ctx.action_class("user")
+class UserActions:
+    # ----- Navigation -----
+
+    # inherited from "file_manager.py"
+    def file_manager_go_parent():
+        actions.user.file_manager_go("..")
+
+    def file_manager_go_home():
+        actions.user.file_manager_go("%USERPROFILE%")
+
+    def file_manager_go(path: str):
+        path = path.replace(" ", "\\ ")
+        actions.insert(f"cd {path}")
+        actions.key("enter")
+
+    # ----- Create folders / files -----
+
+    # inherited from "file_manager.py"
+    def file_manager_new_folder(name: str = None):
+        actions.insert(f"mkdir {name or ''}")
--- apps/terminal/cmd.talon
+++ apps/terminal/cmd.talon
@@ -4,3 +4,9 @@ app: cmd
 tag(): terminal
 
 talon repl:                 insert('"C:\\Program Files\\Talon\\python.exe" "C:\\Program Files\\Talon\\repl.py"')
+
+# TODO: merge the below commands with the other terminals
+tree files:                 "tree /f\n"
+tree folders:               "tree\n"
+
+list:                       "dir\n"
