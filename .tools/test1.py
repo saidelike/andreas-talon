419cb64172d873c8d5bc645ab867263e42b3d0cc
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-03-21 20:10:27 +0000

    feat: example list to convert to talon-list

---
+++ .tools/test1.py
@@ -0,0 +1,14 @@
+ctx.lists["self.git_conventional_commits"] = {
+    "feature": "feat",
+    "feet": "feat",
+    "fix": "fix",
+    "documentation": "docs",
+    "docks": "docs",
+    "style": "style",
+    "refactor": "refactor",
+    "performance": "perf",
+    "test": "test",
+    "chore": "chore",
+    "revert": "revert",
+    "breaking": "BREAKING CHANGE",
+}
